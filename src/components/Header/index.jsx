64903af2b30ebef8f4205482623f57ded91c1786
Leticia Halana Ribeiro de Sousa <leticia.halana.tr@gmail.com>
2025-03-07 18:00:49 -0300

    chore: Mudança no icone e correção do menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,28 @@
-import styles from "./styleHeader.module.css"
+import styles from "./styleHeader.module.css";
 import React, { useState } from "react";
 
 export const Header = () => {
-    const [menuVisivel, setMenuVisivel] = useState(false);
+  const [menuVisivel, setMenuVisivel] = useState(false);
 
-    const alternarVisibilidade = () => {
-        setMenuVisivel((prev) => !prev);
-    };
-    return (
-        <div className={styles.header}>
-            <header className={styles.nav}>
-                <h1>Letícia Halana</h1>
-                <div className={styles.containerVisibleMenu} >
-                    <div className={styles.container} onClick={alternarVisibilidade}></div>
-                    <ul className={styles.conatinerMenu}>
-                        <li className={menuVisivel ? '' : styles.hidden}>Home</li>
-                        <li className={menuVisivel ? '' : styles.hidden}>Pojects</li>
-                        <li className={menuVisivel ? '' : styles.hidden}>Contatos</li>
-                        <li className={menuVisivel ? '' : styles.hidden}>Midias Digitais</li>
-                    </ul>
-                </div>
-            </header>
+  const alternarVisibilidade = () => {
+    setMenuVisivel((prev) => !prev);
+  };
+  return (
+    <div className={styles.header}>
+      <header className={styles.nav}>
+        <div className={styles.containerLogo}></div>
+        <div className={styles.containerVisibleMenu}>
+          <div
+            className={styles.container}
+            onClick={alternarVisibilidade}
+          ></div>
+          <ul className={styles.conatinerMenu}>
+            <li className={menuVisivel ? "" : styles.hidden}>Home</li>
+            <li className={menuVisivel ? "" : styles.hidden}>Pojetos</li>
+            <li className={menuVisivel ? "" : styles.hidden}>Contatos</li>
+          </ul>
         </div>
-    )
-}
+      </header>
+    </div>
+  );
+};
